823a882aea6f394cf5955fab444ccabeb376cd69
Alfred Anyan <46558691+yawaddison@users.noreply.github.com>
2020-09-11 19:04:12 +0200

    loading images

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,9 @@
 #include "Game.h"
 
+
+SDL_Texture* playerTex;
+SDL_Rect srcRectangle, destRectangle;
+
 Game::Game()
 {}
 
@@ -15,33 +19,47 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 	}
 	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
 	{
-		std::cout << "Subsystems Initialized" << std::endl;
+		//std::cout << "Subsystems Initialized" << std::endl;
 		window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-		if (window)
+		/*if (window)
 		{
 			std::cout << "Window created!" << std::endl;
 
-		}
+		}*/
 
 		renderer = SDL_CreateRenderer(window, -1, 0);
 		if (renderer)
 		{
 			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-			std::cout << "Renderer created!" << std::endl;
+			//std::cout << "Renderer created!" << std::endl;
 		}
 		isRunning = true;
 	}
-	else {
+	/*else {
 		isRunning = false;
+	}*/
+
+	SDL_Surface* tmpSurface = IMG_Load("assets/flowers.jpg");
+	if (!tmpSurface) {
+		printf("IMG_Load: %s\n", IMG_GetError());
+		// handle error
 	}
+	std::cout << "man loaded" << std::endl;
+	playerTex = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	SDL_FreeSurface(tmpSurface);
+
 
 }
 void Game::update(){
 	cnt++;
-	std::cout << cnt << std::endl;
+	destRectangle.w = 100;
+	destRectangle.h = 100;
+	//std::cout << cnt << std::endl;
 }
 void Game::render(){
 	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, playerTex, NULL, &destRectangle);
+	//std::cout << "man rendered" << std::endl;
 	SDL_RenderPresent(renderer);
 
 }
